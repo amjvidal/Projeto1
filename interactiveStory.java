9883b6cf8d0cd1c907f2a41b5cd4bf0471870a9a
amjvidal <antoniomariojesus123@gmail.com>
2023-06-17 16:48:56 -0300

    inicio

--- interactiveStory.java
+++ interactiveStory.java
@@ -1,12 +1,6 @@
 package P2;
 public class interactiveStory {
     
-public static String print() {
-    System.out.println("");
-    return null;
-}
-
-
 public static void main(String[] args) {
     
 }
