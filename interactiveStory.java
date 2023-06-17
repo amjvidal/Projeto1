f462dc6061e402efa63f5afd447a29dba3a2fc85
amjvidal <antoniomariojesus123@gmail.com>
2023-06-17 17:00:02 -0300

    Tentativa de implementação do metodo pra dar print

--- interactiveStory.java
+++ interactiveStory.java
@@ -1,7 +1,15 @@
 package P2;
 public class interactiveStory {
-    
+
+ public static void printString(String a) {
+        System.out.println(a);
+    }
+
+
 public static void main(String[] args) {
-    
+    String introdução = "Bom dia";
+    printString(introdução);
+    String fala = "Ela disse que eu sou feia";
+    printString(fala);
 }
 }
