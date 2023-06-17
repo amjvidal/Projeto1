95adecfa516d22044f99073d15912a5020c54dcd
amjvidal <antoniomariojesus123@gmail.com>
2023-06-17 16:48:01 -0300

    Initial commit

---
+++ interactiveStory.java
@@ -0,0 +1,13 @@
+package P2;
+public class interactiveStory {
+    
+public static String print() {
+    System.out.println("");
+    return null;
+}
+
+
+public static void main(String[] args) {
+    
+}
+}
